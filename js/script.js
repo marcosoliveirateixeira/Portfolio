83d1c04f72662aaae0c3fcdafd9c8e889c2d739e
Marcos Teixeira <marcos.gabriel96@hotmail.com>
2023-02-07 16:18:49 -0300

    Updating Some skills Sets

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+const skill1Value = document.getElementById("skill-1-value").innerText;
+const skill1Bar = document.getElementById("skill-1");
+
+skill1Bar.style.width = skill1Value;
